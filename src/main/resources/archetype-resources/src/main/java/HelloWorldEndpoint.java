be62f8c3c730fe66ec60ee88c9a2bfe19aa58ef2
Hayri Cicek <hayri@kodnito.com>
2019-01-11 23:41:41 +0100

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/HelloWorldEndpoint.java
@@ -0,0 +1,21 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok("Hello from MicroProfile!").build();
+    }
+}
